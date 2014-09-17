dd7efc861da618c698e6a8b78cb5f9ec5df85865
ryancheunggit <ryanzjlib@gmail.com>
2014-09-17 20:19:24 +0800

    fixing bug

--- Graph/PA2.py
+++ Graph/PA2.py
@@ -9,11 +9,13 @@ import random
 # Breadth-first search
 
 def bfs_visited(ugraph, start_node):
+	# input: undirected graph represented as adj list, a starting node
+	# output: a set containing the nodes connected to the starting node
 	queue = [start_node]
 	visited = [start_node]
 	while queue != []:
-		j = queue.pop()
-		for neighbor in ugraph[j]:
+		node = queue.pop()
+		for neighbor in ugraph[node]:
 			if neighbor not in visited:
 				visited.append(neighbor)
 				queue.append(neighbor)
@@ -25,20 +27,27 @@ def bfs_visited(ugraph, start_node):
 
 # Connected components
 def cc_visited(ugraph):
+	# input: undirected graph represented as adj list
+	# output: list of sets, each set represents a connected component
 	remaining_nodes = ugraph.keys()
 	connected_components = []
 	while remaining_nodes != []:
-		i = remaining_nodes[random.randint(0,len(remaining_nodes)-1)]
-		component = bfs_visited(ugraph, i)
+		node = remaining_nodes[random.randint(0,len(remaining_nodes)-1)]
+		component = bfs_visited(ugraph, node)
 		connected_components.append(component)
-		remaining_nodes = [node for node in remaining_nodes if node not in component]
+		remaining_nodes = [remain for remain in remaining_nodes if remain not in component]
 	return connected_components
 
 # cc_visited(EX_GRAPH1)
 
 # Largest Connected component size
 def largest_cc_size(ugraph):
+	# input: undirected graph represented as adj list
+	# output: integer representing the size of the largest connected component
 	connected_components = cc_visited(ugraph)
+	length = [len(component) for component in connected_components]
+	if length == []:
+		return 0
 	return max([len(component) for component in connected_components])
 
 # largest_cc_size(EX_GRAPH1)	
@@ -46,6 +55,8 @@ def largest_cc_size(ugraph):
 # Graph resilience
 
 def compute_resilience(ugraph, attack_order):
+	# input: undirected graph represented as adj list, list of nodes to be removed from graph
+	# output, list of the size of the largest connected component in the remaining graph
 	remaining_largest_cc_size = [largest_cc_size(ugraph)]
 	for node in attack_order:
 		ugraph.pop(node)

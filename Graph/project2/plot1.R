ed12ec1285e6552dfd9c8e82390a8664f13bb419
ryancheunggit <ryanzjlib@gmail.com>
2014-09-21 17:21:08 +0800

    recreate plot

--- Graph/project2/plot1.R
+++ Graph/project2/plot1.R
@@ -7,7 +7,7 @@ number_of_nodes_removed <- as.vector(c(seq(0,1347),
                                        seq(0,1347),
                                        seq(0,1347)))
 resilience <- as.vector(rbind(resiliences,erresiliences,uparesiliences))
-factors <- as.vector(c(rep('computer_network',1348),
+graph <- as.vector(c(rep('computer_network',1348),
              rep('er p=0.001716433045422606',1348),
              rep('upa m =2',1348)))
 
@@ -15,7 +15,7 @@ df <- as.data.frame(cbind(number_of_nodes_removed,resilience,factors))
 
 png("plot1.png", width = 800, height = 600)
 ggplot(df, aes(x = number_of_nodes_removed, y = V1)) +
-  geom_line(aes(group = factors, color= factors), size = 1, aplha = 0.5) +
+  geom_line(aes(group = graph, color= graph), size = 1, aplha = 0.5) +
   labs(title = "Network Resiliences", 
        x= "Number of Nodes Removed", 
        y = "Size of the Largest Connected Component")

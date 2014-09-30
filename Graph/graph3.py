ee14233f1d0a3072f9a47dc6365f6b93093be220
Ryan Cheung <ryanzjlib@gmail.com>
2014-09-30 22:56:32 +0800

    Update graph3.py

--- Graph/graph3.py
+++ Graph/graph3.py
@@ -1,9 +1,9 @@
 def countInversion(array):
-  ```
+  '''
   count number of inversions in the array, and perform sorting with smallest first
   input: array
   output: number of inversions in the array, the original array will be sorted
-  ```
+  '''
   n = len(array)
   if n == 1:
     return 0
@@ -14,11 +14,12 @@ def countInversion(array):
     countRight = countInversion(right_array)
     countMerge = merge(left_array,right_array,array)
     return countLeft+countRight+countMerge
-  
+ 
+
 def merge(left_array,right_array,array):
-  ```
+  '''
   merge function for countInversion(array)
-  ```
+  '''
   count = 0
   p, q = len(left_array), len(right_array)
   i, j, k = 0, 0, 0
@@ -37,10 +38,11 @@ def merge(left_array,right_array,array):
     array[k:] = left_array[i:]
   return count
 
+
 def mystery(array,left_boundary,right_boundary):
-  ```
+  '''
   return i if there exists an i such that array[i] == i, and -1 otherwise
-  ```
+  '''
   if left_boundary > right_boundary:
     return -1
   middle = (left_boundary + right_boundary)/2
@@ -52,6 +54,21 @@ def mystery(array,left_boundary,right_boundary):
     else:
       return mystery(array, left_boundary, middle - 1)
 
+    
+def hierarchicalClustering(points, k):
+  '''
+  input: points whose ith point pi is a pair(xi,yi), and k, the desired number of cluster
+  output: k clusters
+  '''
+  n = len(points)
+  clusters = [{i} for i in points.keys()]
+  while len(clusters) > k:
+    i, j = closestPair(clusters)
+    clusters.append(i.union(j))
+    clusters.remove(i)
+    clusters.remove(j)
+  return clusters
+
 
 def euclidianDistance(p1, p2):
   '''
@@ -59,7 +76,7 @@ def euclidianDistance(p1, p2):
   output: euclidian distance between the two points
   '''
   return sum([(p1[i] - p2[i])**2 for i in range(len(p1))])**0.5
-
+  
 
 def BFClosestPair(points):
   '''
@@ -77,3 +94,29 @@ def BFClosestPair(points):
         smallestI = i
         smallestJ = j
   return (smallestDistance, smallestI, smallestJ)
+  
+
+def SlowDCClosestPair(points):
+  '''
+  a divide-and-conquer algorithm solving the closest pair problem
+  input: a set p of more than 2 points
+  output: a tuple(d,i,j) where d is the smallest pairwise distance of points in p and i,j are the indices of two points
+  '''
+  n = len(points)
+  if n <= 3:
+    return BFClosestPair(points)
+  else:
+    return 0
+    
+  
+#A = [5,4,3,2,1,0]
+#print countInversion(A)
+#print A
+
+#A = [5,4,6,3,7,1]
+#print mystery(A, 0, 6)
+
+#points = {0:(1,1),1:(1,-0.8),2:(-0.5,-0.5),3:(-0.3,1.2)}
+#p1 = (1,2)
+#p2 = (2,3)
+#print euclidianDistance(p1,p2)
